b1ce1eb615b56c7a5389236a9e27ec88a451ef42
multiverse-95 <vyvalkov@yandex.ru>
2022-07-10 15:31:28 +0700

    #1 Added Carousel

--- src/components/mainPage/mainTitle/MainTitle.tsx
+++ src/components/mainPage/mainTitle/MainTitle.tsx
@@ -1,11 +1,38 @@
 import React from "react";
-import {Col, Container, Row} from 'react-bootstrap';
+import {Col, Container, Row, Carousel} from 'react-bootstrap';
 
 class MainTitle extends React.Component{
     render() {
         return(
             <div className="main-title">
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                            height="450"
+                            className="d-block w-100"
+                            src={require('../../../assets/images/carouselImages/msi-geforce-rtx-3090-ti-suprim-x-1.jpg')}
+                            alt="First slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            height="450"
+                            className="d-block w-100"
+                            src={require('../../../assets/images/carouselImages/news.jpg')}
+                            alt="Second slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            height="450"
+                            className="d-block w-100"
+                            src={require('../../../assets/images/carouselImages/calc.jpg')}
+                            alt="Third slide"
+                        />
 
+
+                    </Carousel.Item>
+                </Carousel>
             </div>
         )
     }
